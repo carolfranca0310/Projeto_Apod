9e6a064aee5bd7c6bad7d9c8d1001e62398b9946
Caroline <carolfranca0310@gmail.com>
2022-05-25 15:05:21 -0300

    alteração do JS, inserção de jquery, alteração na estilização, consumo da API

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,12 @@
+
 function ChamadaAPI(date) {
     let URL = "https://api.nasa.gov/planetary/apod?";
-    chave ="Ey7AxDfg7ssx5wm2kcUBrKWSXPnovnkwGxuoKSzV";
-    
+    chave = "Ey7AxDfg7ssx5wm2kcUBrKWSXPnovnkwGxuoKSzV";
+
+
     fetch(`${URL}api_key=${chave}&date=${date}`) // fetch = função para chamar a API
         .then(resp => resp.json())
-        .then(function(retorno) {
-            console.log(retorno)
+        .then(function (response) {
+            ProcessarHTML(response);
         })
 }
-
-function creatNode(element) {
-    return document.createElement(element);
-}
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-var result = ChamadaAPI("2014-10-01")
